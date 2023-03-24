a36af5ea55c0e7bc4aa54748becab0239c7be66e
Ari Ketola <ariketola@hotmail.com>
2023-03-24 17:35:17 +0100

    Gjort ytterligare lite städning, nu blir det helg för mig.

--- Program.cs
+++ Program.cs
@@ -26,59 +26,56 @@ namespace MJU23v_D10_inl_sveng
             do
             {
                 Console.Write("> ");
-                string[] argument = Console.ReadLine().Split();
-                string command = argument[0];
-                if (command == "quit")
-                { Console.WriteLine("Goodbye!"); break; }
+                string[] commandLine = Console.ReadLine().Split();
+                string command = commandLine[0];
+                if (command == "quit" || command == "q")
+                { Console.WriteLine("Goodbye!"); return; }
                 else if (command == "load")
                 {
-                    if (argument.Length == 1) loadList(defaultFile);
+                    if (commandLine.Length == 1) loadList(defaultFile);
                     else try
-                        { loadList("..\\..\\..\\dict\\" + argument[1]); }
-                        catch (System.IO.FileNotFoundException) { Console.WriteLine($"File ..\\..\\..\\dict\\{argument[1]} didn´t exist"); }
+                        { loadList("..\\..\\..\\dict\\" + commandLine[1]); }
+                        catch (System.IO.FileNotFoundException) { Console.WriteLine($"File ..\\..\\..\\dict\\{commandLine[1]} didn´t exist"); }
                 }
                 else if (command == "list")
                 {
                     try
-                    {
-                        foreach (SweEngGloss gloss in dictionary)
-                        { Console.WriteLine($"{gloss.word_swe,-10}  - {gloss.word_eng,-10}"); }
-                    }
+                    {   foreach (SweEngGloss gloss in dictionary)
+                        { Console.WriteLine($"{gloss.word_swe,-10}  - {gloss.word_eng,-10}"); } }
                     catch (System.NullReferenceException) { Console.WriteLine($"Empty list, load a list before using this command"); }
                 }
                 else if (command == "new")
                 {
-                    wordCheck(argument, out string newSweWord, out string newEngWord);
-                    if (argument.Length == 1 || argument.Length == 3)
+                    wordCheck(commandLine, out string newSweWord, out string newEngWord);
+                    if (commandLine.Length == 1 || commandLine.Length == 3)
                         try { dictionary.Add(new SweEngGloss(newSweWord, newEngWord)); Console.WriteLine($"{newSweWord} and {newEngWord} added to list "); }
                         catch (System.NullReferenceException) { Console.WriteLine($"Empty list, load a list before using this command"); }
                 }
                 else if (command == "delete")
                 {
-                    wordCheck(argument, out string delSweWord, out string delEngWord);
-                    if (argument.Length == 1 || argument.Length == 3)
+                    wordCheck(commandLine, out string delSweWord, out string delEngWord);
+                    if (commandLine.Length == 1 || commandLine.Length == 3)
                         try { removeWord(delSweWord, delEngWord); }
                         catch (System.NullReferenceException) { Console.WriteLine($"Empty list, load a list before using this command"); }
                 }
                 else if (command == "translate")
                 {
-                    if (argument.Length == 2)
-                    {
-                        TranslateWord(argument[1]);
-                    }
-                    else if (argument.Length == 1)
+                    if (commandLine.Length == 2)
+                    { TranslateWord(commandLine[1]); }
+                    else if (commandLine.Length == 1)
                     {
                         Console.Write("Write word to be translated: ");
                         string wordToTranslate = Console.ReadLine();
                         TranslateWord(wordToTranslate);
                     }
-                    else { Console.WriteLine("Wrong input. Use translate and push enter"); }
+                    else { Console.WriteLine("Wrong input. Write translate and press enter"); }
                 }
                 else if (command == "help")
                 {
                     Console.WriteLine("Allowed commands:");
-                    Console.Write("quit - for end this program\nload - load a file\nlist - list all posts in program\nnew - " +
-                                  "add new post\ndelete - delete post\ntranslate - translate av word \n ");
+                    Console.Write("<quit> - end this program\n<load> - load a file\n<load> <filename> - load a specific file\n<list> - list all posts in program\n<new> - " +
+                                  "add new post\n<new> <swedish word> <english word> - add new post\n<delete> - delete post\n<delete> <swedish word> <english word> - " +
+                                  "delete specific post\n<translate> - translate a word \n<translate> <word> - translate a specific word \n ");
                 }
                 else
                 {
@@ -87,19 +84,18 @@ namespace MJU23v_D10_inl_sveng
             }
             while (true);
         }
-        private static void wordCheck(string[] argument, out string sweWord, out string engWord)
+        private static void wordCheck(string[] commandLine, out string sweWord, out string engWord)
         {
-            if (argument.Length == 3) { sweWord = argument[1]; engWord = argument[2]; }
-            else if (argument.Length == 1)
+            if (commandLine.Length == 3) { sweWord = commandLine[1]; engWord = commandLine[2]; }
+            else if (commandLine.Length == 1)
             {
                 Console.Write("Write word in Swedish: ");
                 sweWord = Console.ReadLine();
                 Console.Write("Write word in English: ");
                 engWord = Console.ReadLine();
             }
-            else { Console.WriteLine("Wrong input. Use new or delete and push enter"); sweWord = ""; engWord = ""; }
+            else { Console.WriteLine("Wrong input. <help> for help"); sweWord = ""; engWord = ""; }
         }
-
         private static void loadList(string defaultFile)
         {
             using (StreamReader sr = new StreamReader(defaultFile))
@@ -114,7 +110,6 @@ namespace MJU23v_D10_inl_sveng
                 }
             }
         }
-
         private static void removeWord(string sweWord, string engWord)
         {
             int index = -1;
@@ -130,22 +125,20 @@ namespace MJU23v_D10_inl_sveng
             }
             catch (System.ArgumentOutOfRangeException) { Console.WriteLine($"{sweWord} and/or {engWord} doesn´t excist in dictionary"); }
         }
-        private static void TranslateWord(string argument)
+        private static void TranslateWord(string word)
         {
             try
-            {
-                int wordexist = 0;
+            {   int wordexist = 0;
                 foreach (SweEngGloss gloss in dictionary)
                 {
-                    if (gloss.word_swe == argument)
+                    if (gloss.word_swe == word)
                     { Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}"); wordexist++; }
-                    if (gloss.word_eng == argument)
+                    if (gloss.word_eng == word)
                     { Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}"); wordexist++; }
                 }
-                if (wordexist == 0) Console.WriteLine($"The word {argument} doesn´t exist in dictionary");
+                if (wordexist == 0) Console.WriteLine($"The word {word} doesn´t exist in dictionary");
             }
             catch (System.NullReferenceException) { Console.WriteLine($"Empty list, load a list before using this command"); }
-
         }
     }
 }
