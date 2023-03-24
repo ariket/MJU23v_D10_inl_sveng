3043a1528754b4a9c56b376db2f41b21e2d9232a
Ari Ketola <ariketola@hotmail.com>
2023-03-24 09:56:16 +0100

    Enbokstavsvariabler har döpt om

--- Program.cs
+++ Program.cs
@@ -76,10 +76,10 @@
                     else if(argument.Length == 1)
                     {
                         Console.WriteLine("Write word in Swedish: ");
-                        string s = Console.ReadLine();
+                        string sweWord = Console.ReadLine();
                         Console.Write("Write word in English: ");
-                        string e = Console.ReadLine();
-                        dictionary.Add(new SweEngGloss(s, e));
+                        string engWord = Console.ReadLine();
+                        dictionary.Add(new SweEngGloss(sweWord, engWord));
                     }
                 }
                 else if (command == "delete")  //TODO kontrollera att inmatning av användare är korrekt
@@ -97,14 +97,14 @@
                     else if (argument.Length == 1)
                     {
                         Console.WriteLine("Write word in Swedish: ");
-                        string s = Console.ReadLine();
+                        string sweWord = Console.ReadLine();
                         Console.Write("Write word in English: ");
-                        string e = Console.ReadLine();
+                        string engWord = Console.ReadLine();
                         int index = -1;
                         for (int i = 0; i < dictionary.Count; i++)
                         {
                             SweEngGloss gloss = dictionary[i];
-                            if (gloss.word_swe == s && gloss.word_eng == e)
+                            if (gloss.word_swe == sweWord && gloss.word_eng == engWord)
                                 index = i;
                         }
                         dictionary.RemoveAt(index);
@@ -125,12 +125,12 @@
                     else if (argument.Length == 1)
                     {
                         Console.WriteLine("Write word to be translated: ");
-                        string s = Console.ReadLine();
+                        string wordToTranslate = Console.ReadLine();
                         foreach (SweEngGloss gloss in dictionary)
                         {
-                            if (gloss.word_swe == s)
+                            if (gloss.word_swe == wordToTranslate)
                                 Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
-                            if (gloss.word_eng == s)
+                            if (gloss.word_eng == wordToTranslate)
                                 Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}");
                         }
                     }
